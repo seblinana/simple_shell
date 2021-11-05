2a98c131314b55feb6275d63cb387f862dbd0bfc
seblewongel petros <seblinana25@gmail.com>
2021-11-06 02:53:58 +0300

    creating shell files

---
+++ simple_shell.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/waith>
+#include <stdlib.h>
+
+/**
+ * main - builds a simple shell
+ * 
+ * */
+
+int main(int argc, char **argv)
+{
+    char *cmd;
+    do
+    {
+        print_prompt1();
+        cmd = read_cmd();
+        if(!cmd)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+        {
+            free(cmd);
+            continue;
+        }
+        if(strcmp(cmd, "exit\n") == 0)
+        {
+            free(cmd);
+            break;
+        }
+        printf("%s\n", cmd);
+        free(cmd);
+    } while(1);
+    exit(EXIT_SUCCESS);
+}
